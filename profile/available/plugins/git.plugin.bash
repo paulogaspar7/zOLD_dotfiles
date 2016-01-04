7cbaa03dc8907507a9ddf1afc127a01d73e8e834
paulogaspar <paulo.gaspar@klarna.com>
2016-01-04 10:50:37 +0000

    Git stats.

--- profile/available/plugins/git.plugin.bash
+++ profile/available/plugins/git.plugin.bash
@@ -173,6 +173,123 @@ function g.stats {
 # including some modifications
 
 	if [ -n "$(git symbolic-ref HEAD 2> /dev/null)" ]; then
+		LOGOPTS=()
+		END_AND_BEGIN=()
+		#argument parsing
+		while [ -n "$1" ]; do
+		    case "$1" in
+		     "-s")
+		        shift
+		        END_AND_BEGIN+=("--after=$1")
+		    ;;
+		    "-e")
+		        shift
+		        END_AND_BEGIN+=("--before=$1")
+		    ;;
+		    "-w")
+		        LOGOPTS+=("-w")
+		    ;;
+		    "-C")
+		        LOGOPTS+=("-C")
+				LOGOPTS+=("--find-copies-harder")
+		    ;;
+		    "-M")
+		        LOGOPTS+=("-M")
+		    ;;
+		    esac
+		    shift
+		done
+
+		#test if the directory is a git
+		git branch &> /dev/null || exit 3
+		echo "Number of commits per author:"
+		git --no-pager shortlog "${END_AND_BEGIN[@]}" -sn --all
+		AUTHORS=$(git shortlog "${END_AND_BEGIN[@]}" -sn --all | cut -f2 | cut -f1 -d' ')
+		
+		echo .
+		echo .
+		echo "$AUTHORS"
+		echo .
+		echo .
+
+		for a in $AUTHORS
+		do
+		    echo '-------------------'
+		    echo "Statistics for: $a"
+		    echo -n "Number of files changed: "
+		    git log "${LOGOPTS[@]}" "${END_AND_BEGIN[@]}" --all --numstat --format="%n" --author="$a" | grep -v -e "^$" | cut -f3 | sort -iu | wc -l
+		    echo -n "Number of lines added: "
+		    git log "${LOGOPTS[@]}" "${END_AND_BEGIN[@]}" --all --numstat --format="%n" --author="$a" | cut -f1 | awk '{s+=$1} END {print s}'
+		    echo -n "Number of lines deleted: "
+		    git log "${LOGOPTS[@]}" "${END_AND_BEGIN[@]}" --all --numstat --format="%n" --author="$a" | cut -f2 | awk '{s+=$1} END {print s}'
+		    echo -n "Number of lines diff: "
+		    git log "${LOGOPTS[@]}" "${END_AND_BEGIN[@]}" --all --numstat --format="%n" --author="$a" | cut -f1 -f2 | awk '{s+=$1-$2} END {print s}'
+		    echo -n "Number of merges: "
+		    git log "${LOGOPTS[@]}" "${END_AND_BEGIN[@]}" --all --merges --author="$a" | grep -c '^commit'
+
+		done
+	else
+		echo "you're currently not in a git repository"
+	fi
+}
+
+function g.stats.loc2 {
+	about 'display stats per author'
+	group 'git'
+
+# awesome work from https://github.com/esc/git-stats
+# including some modifications
+
+	if [ -n "$(git symbolic-ref HEAD 2> /dev/null)" ]; then
+		LOGOPTS=()
+		END_AND_BEGIN=()
+		#argument parsing
+		while [ -n "$1" ]; do
+		    case "$1" in
+		     "-s")
+		        shift
+		        END_AND_BEGIN+=("--after=$1")
+		    ;;
+		    "-e")
+		        shift
+		        END_AND_BEGIN+=("--before=$1")
+		    ;;
+		    "-w")
+		        LOGOPTS+=("-w")
+		    ;;
+		    "-C")
+		        LOGOPTS+=("-C")
+				LOGOPTS+=("--find-copies-harder")
+		    ;;
+		    "-M")
+		        LOGOPTS+=("-M")
+		    ;;
+		    esac
+		    shift
+		done
+
+		#test if the directory is a git
+		git branch &> /dev/null || exit 3
+		AUTHORS=$(git shortlog "${END_AND_BEGIN[@]}" -sn --all | cut -f2)
+
+		for a in $AUTHORS
+		do
+			echo "=== $a ==="
+		    git log "${LOGOPTS[@]}" "${END_AND_BEGIN[@]}" --all --numstat --format="%n" --author="$a" | cut -f1 -f2 | awk '{s+=$1-$2} END {print s " " a}' "a=$a" "$@"
+		done
+	else
+		echo "you're currently not in a git repository"
+	fi
+}
+
+function g.stats.old {
+	about 'display stats per author'
+	group 'git'
+
+# awesome work from https://github.com/esc/git-stats
+# including some modifications
+
+	if [ -n "$(git symbolic-ref HEAD 2> /dev/null)" ]; then
 		echo "Number of commits per author:"
 		git --no-pager shortlog -sn --all
 		AUTHORS=$( git shortlog -sn --all | cut -f2 | cut -f1 -d' ')
